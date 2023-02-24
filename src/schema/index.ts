d91ed8e7085cc1e6a3c7bc0414ba03dcf9c481d2
[AI6/1반] 김범진 <qjawls646@gmail.com>
2023-02-24 15:12:51 +0900

    uri 수정

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -5,8 +5,8 @@ const connect = () => {
   if (process.env.NODE_ENV !== 'production') {
     mongoose.set('debug', true);
   }
-  const username = encodeURIComponent('uouProject');
-  const password = encodeURIComponent(process.env.MONGO_PASSWORD !== undefined && process.env.MONGO_PASSWORD);
+  const username = 'uouProject';
+  const password = process.env.MONGO_PASSWORD;
   const uri = `mongodb+srv://${username}:${password}@cluster0.jv9pc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
   mongoose.connect(uri, { dbName: 'rewind-express' }, (error) => {
     if (error) {
