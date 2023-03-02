c737978f9912808654d82b152255d51b773a16b7
[AI6/1반] 김범진 <qjawls646@gmail.com>
2023-03-02 19:21:20 +0900

    상대경로 수정

--- src/routes/post.ts
+++ src/routes/post.ts
@@ -10,7 +10,7 @@ import {
   updatePost,
   getPostTags,
 } from '../controller/post';
-import upload from 'src/multer';
+import upload from '../../src/multer';
 import auth from '../middleware/auth';
 
 const router = Router();
