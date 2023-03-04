a5c40206e577160b46a29d80c0df1285e368e151
[AI6/1반] 김범진 <qjawls646@gmail.com>
2023-03-04 14:45:12 +0900

    add base url

--- src/controller/post.ts
+++ src/controller/post.ts
@@ -58,7 +58,7 @@ export const createPost = async (req: Request, res: Response) => {
     const newPost = await Post.create({
       author,
       content,
-      imgUrl: files === undefined ? [] : files.map((file) => file.filename),
+      imgUrl: files === undefined ? [] : files.map((file) => `http://13.112.212.213/api/uploads/${file.filename}`),
       tags: JSON.parse(tags),
     });
 
@@ -90,7 +90,11 @@ export const updatePost = async (req: Request, res: Response) => {
   const files = req.files as Express.Multer.File[];
   try {
     const post = await Post.findByIdAndUpdate(postId, {
-      $set: { content, imgUrl: files === undefined ? [] : files.map((file) => file.filename), tags: JSON.parse(tags) },
+      $set: {
+        content,
+        imgUrl: files === undefined ? [] : files.map((file) => `http://13.112.212.213/api/uploads/${file.filename}`),
+        tags: JSON.parse(tags),
+      },
     });
     post?.tags.forEach(async (tag) => {
       await Tag.findOneAndUpdate(
